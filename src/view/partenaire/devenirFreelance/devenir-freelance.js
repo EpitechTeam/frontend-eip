8fcecdd896a40a919e21eaf07fa892dcbf5f8464
Fluorz <contact.fluorz@gmail.com>
2019-11-15 16:46:32 +0100

    ADD : devenir freelance

--- src/view/partenaire/devenirFreelance/devenir-freelance.js
+++ src/view/partenaire/devenirFreelance/devenir-freelance.js
@@ -1,11 +1,252 @@
 import React from 'react'
+import './devenirFreelance.css'
 
 class DevenirFreelance extends React.Component {
     render() {
         return (
             <div>
-            landing page freelance
-            </div>
+            <div className="specialContainerDevenirFreelance">
+                    <div className="view lighten-3 setHeightDevenirFreelance">
+                    <div className="mask">
+                        <div className="container h-100">
+                        <div className="row align-items-center h-100">
+                            <div className="col-md-6">
+                            <h1 className="mb-4">Devenez <span className="text-warning">le meilleur</span><br /><span className="cyan-text">freelance</span> de votre région</h1>
+                            <p className="mb-4 pb-2 dark-grey-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam vitae, culpa qui officia deserunt laborum fuga similique mollit id quos aperiam proident non ut rerum debitis.</p>
+                            <button type="button" className="btn btn-primary btn-rounded btn-md ml-md-0">Get started</button>
+                            <button type="button" className="btn btn-outline-grey btn-rounded btn-md">Get started</button>
+                            </div>
+                            <div className="col-md-6">
+                            <img src="https://mdbootstrap.com/img/illustrations/hiker-man-colour.svg" alt="" className="img-fluid" />
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
+                    <div className="dark-grey-text text-center">
+                    <h3 className="font-weight-bold pt-5 pb-2">Lorem ipsum dolor sit amet</h3>
+                    <div className="row mx-3">
+                      <div className="col-md-4 px-4 mb-4">
+                        <div className="view">
+                          <img src="https://mdbootstrap.com/img/illustrations/drawkit-drawing-man-colour.svg" className="img-fluid" alt="smaple image" />
+                        </div>
+                      </div>
+                      <div className="col-md-4 px-4 mb-4">
+                        <div className="view">
+                          <img src="https://mdbootstrap.com/img/illustrations/drawkit-phone-conversation-colour.svg" className="img-fluid" alt="smaple image" />
+                        </div>
+                      </div>
+                      <div className="col-md-4 px-4 mb-4">
+                        <div className="view">
+                          <img src="https://mdbootstrap.com/img/illustrations/app-user-colour.svg" className="img-fluid" alt="smaple image" />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                    <div className="text-center px-md-5 mx-md-5 dark-grey-text">
+                    <div className="row mb-5">
+                        <div className="col-md-4 mx-auto">
+                        <div className="view mb-4 pb-2">
+                            <img src="https://mdbootstrap.com/img/illustrations/undraw_connected_world_wuay.svg" className="img-fluid" alt="smaple image" />
+                        </div>
+                        </div>
+                    </div>
+                    <h3 className="font-weight-bold mb-4 pb-2">Lorem ipsum dolor sit amet</h3>
+                    <p className="text-center mx-auto mb-4 pb-2">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore et quasi architecto explicabo.</p>
+                    <button type="button" className="btn btn-deep-purple btn-rounded">Design your site now</button>
+                    </div>
+
+                    <div>
+                    <div className="row">
+                        <div className="col-lg-7 mb-4 align-self-center text-center text-lg-left">
+                        <h2 className="h2 mb-5">Notre application</h2>
+                        <p className="text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam eum porro a pariatur veniam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum.</p>
+                        <button type="button" className="btn btn-cyan waves-effect btn-md ml-lg-0"><i className="fab fa-apple fa-lg pr-1" /> App Store</button>
+                        <button type="button" className="btn btn-cyan waves-effect btn-md"><i className="fab fa-google-play fa-lg pr-1" /> Google Play</button>
+                        </div>
+                        <div className="col-lg-5 mb-4">
+                        <div className="view overlay">
+                            <img className="img-fluid mx-auto" src="https://mdbootstrap.com/img/illustrations/app-user-colour.svg" alt="Sample image" />
+                            <a>
+                            <div className="mask rgba-white-slight" />
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div className="team-section text-center dark-grey-text">
+                    <h3 className="font-weight-bold mb-4 pb-2">Testimonials</h3>
+                    <p className="text-center w-responsive mx-auto mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        Fugit, error amet numquam iure provident voluptate esse quasi, veritatis totam voluptas nostrum quisquam
+                        eum porro a pariatur veniam.</p>
+                    <div className="row text-center">
+                        <div className="col-md-4 mb-4">
+                        <div className="testimonial">
+                            <div className="avatar mx-auto">
+                            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(1).jpg" className="rounded-circle z-depth-1 img-fluid" />
+                            </div>
+                            <h4 className="font-weight-bold dark-grey-text mt-4">Anna Deynah</h4>
+                            <h6 className="font-weight-bold blue-text my-3">Web Designer</h6>
+                            <p className="font-weight-normal dark-grey-text">
+                            <i className="fas fa-quote-left pr-2" />Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod
+                            eos id officiis hic tenetur quae quaerat ad velit ab hic tenetur.</p>
+                            <div className="orange-text">
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star-half-alt"> </i>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-md-4 mb-4">
+                        <div className="testimonial">
+                            <div className="avatar mx-auto">
+                            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(8).jpg" className="rounded-circle z-depth-1 img-fluid" />
+                            </div>
+                            <h4 className="font-weight-bold dark-grey-text mt-4">John Doe</h4>
+                            <h6 className="font-weight-bold blue-text my-3">Web Developer</h6>
+                            <p className="font-weight-normal dark-grey-text">
+                            <i className="fas fa-quote-left pr-2" />Ut enim ad minima veniam, quis nostrum exercitationem ullam
+                            corporis suscipit laboriosam, nisi ut aliquid commodi.</p>
+                            <div className="orange-text">
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-md-4 mb-4">
+                        <div className="testimonial">
+                            <div className="avatar mx-auto">
+                            <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg" className="rounded-circle z-depth-1 img-fluid" />
+                            </div>
+                            <h4 className="font-weight-bold dark-grey-text mt-4">Maria Kate</h4>
+                            <h6 className="font-weight-bold blue-text my-3">Photographer</h6>
+                            <p className="font-weight-normal dark-grey-text">
+                            <i className="fas fa-quote-left pr-2" />At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                            blanditiis praesentium voluptatum deleniti atque corrupti.</p>
+                            <div className="orange-text">
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            <i className="fas fa-star"> </i>
+                            <i className="far fa-star"> </i>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+  
+                    <div className="dark-grey-text">
+                    <div className="row pr-lg-5">
+                        <div className="col-md-7 mb-4">
+                        <div className="view">
+                            <img src="https://mdbootstrap.com/img/illustrations/graphics(4).png" className="img-fluid" alt="smaple image" />
+                        </div>
+                        </div>
+                        <div className="col-md-5 d-flex align-items-center">
+                        <div>
+                            <h3 className="font-weight-bold mb-4">Restez indépendant</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adip elit. Maiores deleniti explicabo voluptatem quisquam nulla asperiores aspernatur aperiam voluptate et consectetur minima delectus, fugiat eum soluta blanditiis adipisci, velit dolore magnam.</p>
+                            <button type="button" className="btn btn-orange btn-rounded mx-0">Download</button>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+
+                    <div>
+                    <div className="white-text grey p-5 rounded">
+                        <h3 className="text-center font-weight-bold mb-4 pb-2">Counter</h3>
+                        <div className="row">
+                        <div className="col-md-4 mb-4">
+                            <div className="row">
+                            <div className="col-6 pr-0">
+                                <h4 className="display-4 text-right mb-0 count-up" data-from={0} data-to={42} data-time={2000}>42</h4>
+                            </div>
+                            <div className="col-6">
+                                <p className="text-uppercase font-weight-normal mb-1">Projects</p>
+                                <p className="mb-0"><i className="fas fa-briefcase fa-2x mb-0" /></p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 mb-4">
+                            <div className="row">
+                            <div className="col-6 pr-0">
+                                <h4 className="display-4 text-right mb-0 count1" data-from={0} data-to={3500} data-time={2000}>3,500</h4>
+                            </div>
+                            <div className="col-6">
+                                <p className="text-uppercase font-weight-normal mb-1">Customers</p>
+                                <p className="mb-0"><i className="fas fa-user fa-2x mb-0" /></p>
+                            </div>
+                            </div>
+                        </div>
+                        <div className="col-md-4 mb-4">
+                            <div className="row">
+                            <div className="col-6 pr-0">
+                                <h4 className="display-4 text-right"><span className="d-flex justify-content-end"><span className="count2" data-from={0} data-to={100} data-time={2000}>0</span> %</span></h4>
+                            </div>
+                            <div className="col-6">
+                                <p className="text-uppercase font-weight-normal mb-1">Satisfaction</p>
+                                <p className="mb-0"><i className="fas fa-smile fa-2x mb-0" /></p>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+
+                    <div className="specialContainerDevenirFreelance">
+                        <div className="dark-grey-text p-5">
+                        <div className="row">
+                        <div className="col-md-5 mb-4 mb-md-0">
+                            <div className="view">
+                            <img src="https://mdbootstrap.com/img/illustrations/undraw_Group_chat_unwm.svg" className="img-fluid" alt="smaple image" />
+                            </div>
+                        </div>
+                        <div className="col-md-7 mb-lg-0 mb-4">
+                            <form className action>
+                            <h3 className="font-weight-bold my-3">Inscription</h3>
+                            <p className="text-muted mb-4 pb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam vitae, fuga similique quos aperiam tenetur quo ut rerum debitis.</p>
+                            <div className="row d-flex justify-content-center">
+                                    <div className="col-md-6 col-lg-3 mb-4">
+                                    <div className="md-form md-outline form-lg">
+                                        <input type="text" id="form1" className="form-control form-control-lg" />
+                                        <label htmlFor="form1">Name</label>
+                                    </div>
+                                    </div>
+                                    <div className="col-md-6 col-lg-3 mb-4">
+                                    <div className="md-form md-outline form-lg">
+                                        <input type="text" id="form2" className="form-control form-control-lg" />
+                                        <label htmlFor="form2">Email</label>
+                                    </div>
+                                    </div>
+                                    <div className="col-md-6 col-lg-3 mb-4">
+                                    <div className="md-form md-outline form-lg">
+                                        <input type="text" id="form3" className="form-control form-control-lg" />
+                                        <label htmlFor="form3">Password</label>
+                                    </div>
+                                    </div>
+                                    <div className="col-md-6 col-lg-3 mb-4">
+                                    <button className="btn btn-block btn-primary my-4">Sign up</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        </div>
+
+                    </div>
+                
+                    </div>
+                    
+                    </div>
+                 </div>
         )
     }
 }
